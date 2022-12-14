765b70e171857c53f13d6455ef58c5c738bf1195
unknown <sean.kain.geo@gmail.com>
2022-12-13 22:37:14 -0500

    forgot to save moving state stub

--- Assets/Scripts/NPC/StateMachine/MovingState.cs
+++ Assets/Scripts/NPC/StateMachine/MovingState.cs
@@ -5,4 +5,18 @@ using UnityEngine;
 public class MovingState : BaseState<GroundMobileCapability>
 {
     public MovingState(GroundMobileCapability capability) : base("Moving", capability) { }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void UpdateLogic()
+    {
+
+    }
+    public override void Exit()
+    {
+        base.Exit();
+    }
 }
